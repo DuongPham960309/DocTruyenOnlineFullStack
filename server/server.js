0b61127afb797277d13361e4ddb176ec8fad32d8
DuongPham960309 <phamduong960309@gmail.com>
2026-03-19 12:28:23 +0700

    update 1

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cros from 'cors';
-import bodyParser from 'body-parser';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 const app = express();
 
@@ -14,149 +13,145 @@ const pool = mysql.createPool({
     connectionLimit: 2
 });
 
-app.use(cros(), bodyParser.json());
+app.use(cros(), express.json());
 
-app.get('/data', (req, res) => {
-    pool.getConnection(async (err, conn) => {
-        let promiseConn = conn.promise();
-        let rows, fields;
-        let queriedData = {lastUpdatedTime: {}, data: {}};
+app.get('/data',async (req, res) => {
+    const conn = await pool.getConnection();
+    let rows, fields;
+    let queriedData = {lastUpdatedTime: {}, data: {}};
 
-        [rows, fields] = await promiseConn.query("SELECT * FROM last_updated_time");
+    [rows, fields] = await conn.query("SELECT * FROM last_updated_time");
 
-        for (let i = 0; i < rows.length; i++)
-        {
-            queriedData.lastUpdatedTime[rows[i].section] = rows[i].time;
-        }
+    for (let i = 0; i < rows.length; i++)
+    {
+        queriedData.lastUpdatedTime[rows[i].section] = rows[i].time;
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, suggested_novels.chapter 
+    [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, suggested_novels.chapter 
+    FROM suggested_novels JOIN novels ON suggested_novels.name = novels.name ORDER BY id`);
+    queriedData.data.suggestedNovels = rows;
+
+    [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, 
+    selected_translation_novels.chapter FROM selected_translation_novels JOIN novels ON selected_translation_novels.name = novels.name 
+    ORDER BY id`);
+    queriedData.data.selectedTranslationNovels = rows;
+
+    [rows, fields] = await conn.query(`SELECT name, novels.before, after, note, current_chapter AS chapter, last_time FROM novels 
+    WHERE first_time != last_time ORDER BY last_time DESC LIMIT 25`);
+    queriedData.data.updatedNovels = rows;
+
+    [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, novels.current_chapter AS 
+    chapter FROM full_novels JOIN novels ON full_novels.name = novels.name ORDER BY id`);
+    queriedData.data.fullNovels = rows;
+
+    [rows, fields] = await conn.query("SELECT image, title FROM short_novels WHERE short_novels.group = 'leftOfShortNovel'");
+    queriedData.data.leftOfShortNovel = rows[0];
+
+    [rows, fields] = await conn.query(`SELECT image, title FROM short_novels WHERE short_novels.group = 'rightOfShortNovels' ORDER 
+    BY id`);
+    queriedData.data.rightOfShortNovels = rows;
+
+    [rows, fields] = await conn.query("SELECT image, title FROM review_novels ORDER BY id");
+    queriedData.data.reviewNovels = rows;
+
+    [rows, fields] = await conn.query("SELECT name AS title, view FROM novels ORDER BY view DESC LIMIT 10");
+    queriedData.data.topGoodNovels = rows;
+
+    [rows, fields] = await conn.query(`SELECT name AS title, view FROM novels WHERE first_time = last_time ORDER BY first_time 
+    DESC LIMIT 10`);
+    queriedData.data.newUpdateNovels = rows;
+
+    [rows, fields] = await conn.query("SELECT name FROM trend_novels_in_month ORDER BY id");
+    queriedData.data.trendNovelsInMonth = [];
+
+    for (let i = 0; i < rows.length; i++)
+    {
+        queriedData.data.trendNovelsInMonth.push(rows[i].name);
+    }
+
+    conn.release();
+    const data = JSON.stringify(queriedData);
+    res.send(data);
+});
+
+app.post('/data',async (req, res) => {
+    const conn = await pool.getConnection();
+    let rows, fields;
+    let queriedData = {lastUpdatedTime: {}, data: {}};
+
+    [rows, fields] = await conn.query("SELECT * FROM last_updated_time");
+
+    for (let i = 0; i < rows.length; i++) {
+        queriedData.lastUpdatedTime[rows[i].section] = rows[i].time;
+    }
+
+    let updatedTime = req.body;
+    let lastUpdatedTime = queriedData.lastUpdatedTime;
+
+    if (updatedTime.suggestedNovels !== lastUpdatedTime.suggestedNovels) {
+        [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, suggested_novels.chapter 
         FROM suggested_novels JOIN novels ON suggested_novels.name = novels.name ORDER BY id`);
         queriedData.data.suggestedNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, 
+    if (updatedTime.selectedTranslationNovels !== lastUpdatedTime.selectedTranslationNovels) {
+        [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, 
         selected_translation_novels.chapter FROM selected_translation_novels JOIN novels ON selected_translation_novels.name = novels.name 
         ORDER BY id`);
         queriedData.data.selectedTranslationNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT name, novels.before, after, note, current_chapter AS chapter, last_time FROM novels 
-        WHERE first_time != last_time ORDER BY last_time DESC LIMIT 25`);
+    if (updatedTime.updatedNovels !== lastUpdatedTime.updatedNovels) {
+        [rows, fields] = await conn.query(`SELECT name, novels.before, after, note, current_chapter AS chapter, last_time FROM 
+        novels WHERE first_time != last_time ORDER BY last_time DESC LIMIT 25`);
         queriedData.data.updatedNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, novels.current_chapter AS 
+    if (updatedTime.fullNovels !== lastUpdatedTime.fullNovels) {
+        [rows, fields] = await conn.query(`SELECT novels.name, novels.before, novels.after, novels.image, novels.current_chapter AS 
         chapter FROM full_novels JOIN novels ON full_novels.name = novels.name ORDER BY id`);
         queriedData.data.fullNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query("SELECT image, title FROM short_novels WHERE short_novels.group = 'leftOfShortNovel'");
+    if (updatedTime.leftOfShortNovel !== lastUpdatedTime.leftOfShortNovel) {
+        [rows, fields] = await conn.query("SELECT image, title FROM short_novels WHERE short_novels.group = 'leftOfShortNovel'");
         queriedData.data.leftOfShortNovel = rows[0];
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT image, title FROM short_novels WHERE short_novels.group = 'rightOfShortNovels' ORDER 
-        BY id`);
+    if (updatedTime.rightOfShortNovels !== lastUpdatedTime.rightOfShortNovels) {
+        [rows, fields] = await conn.query(`SELECT image, title FROM short_novels WHERE short_novels.group = 'rightOfShortNovels' 
+        ORDER BY id`);
         queriedData.data.rightOfShortNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query("SELECT image, title FROM review_novels ORDER BY id");
+    if (updatedTime.reviewNovels !== lastUpdatedTime.reviewNovels) {
+        [rows, fields] = await conn.query("SELECT image, title FROM review_novels ORDER BY id");
         queriedData.data.reviewNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query("SELECT name AS title, view FROM novels ORDER BY view DESC LIMIT 10");
+    if (updatedTime.topGoodNovels !== lastUpdatedTime.topGoodNovels) {
+        [rows, fields] = await conn.query("SELECT name AS title, view FROM novels ORDER BY view DESC LIMIT 10");
         queriedData.data.topGoodNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query(`SELECT name AS title, view FROM novels WHERE first_time = last_time ORDER BY first_time 
+    if (updatedTime.newUpdateNovels !== lastUpdatedTime.newUpdateNovels) {
+        [rows, fields] = await conn.query(`SELECT name AS title, view FROM novels WHERE first_time = last_time ORDER BY first_time 
         DESC LIMIT 10`);
         queriedData.data.newUpdateNovels = rows;
+    }
 
-        [rows, fields] = await promiseConn.query("SELECT name FROM trend_novels_in_month ORDER BY id");
+    if (updatedTime.trendNovelsInMonth !== lastUpdatedTime.trendNovelsInMonth) {
+        [rows, fields] = await conn.query("SELECT name FROM trend_novels_in_month ORDER BY id");
         queriedData.data.trendNovelsInMonth = [];
 
-        for (let i = 0; i < rows.length; i++)
-        {
+        for (let i = 0; i < rows.length; i++) {
             queriedData.data.trendNovelsInMonth.push(rows[i].name);
         }
+    }
 
-        pool.releaseConnection(conn);
-        const data = JSON.stringify(queriedData);
-        res.send(data);
-    });
-});
-
-app.post('/data', (req, res) => {
-    pool.getConnection(async (err, conn) => {
-        let promiseConn = conn.promise();
-        let rows, fields;
-        let queriedData = {lastUpdatedTime: {}, data: {}};
-
-        [rows, fields] = await promiseConn.query("SELECT * FROM last_updated_time");
-
-        for (let i = 0; i < rows.length; i++) {
-            queriedData.lastUpdatedTime[rows[i].section] = rows[i].time;
-        }
-
-        let updatedTime = req.body;
-        let lastUpdatedTime = queriedData.lastUpdatedTime;
-
-        if (updatedTime.suggestedNovels !== lastUpdatedTime.suggestedNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, suggested_novels.chapter 
-            FROM suggested_novels JOIN novels ON suggested_novels.name = novels.name ORDER BY id`);
-            queriedData.data.suggestedNovels = rows;
-        }
-
-        if (updatedTime.selectedTranslationNovels !== lastUpdatedTime.selectedTranslationNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, 
-            selected_translation_novels.chapter FROM selected_translation_novels JOIN novels ON selected_translation_novels.name = novels.name 
-            ORDER BY id`);
-            queriedData.data.selectedTranslationNovels = rows;
-        }
-
-        if (updatedTime.updatedNovels !== lastUpdatedTime.updatedNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT name, novels.before, after, note, current_chapter AS chapter, last_time FROM 
-            novels WHERE first_time != last_time ORDER BY last_time DESC LIMIT 25`);
-            queriedData.data.updatedNovels = rows;
-        }
-
-        if (updatedTime.fullNovels !== lastUpdatedTime.fullNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT novels.name, novels.before, novels.after, novels.image, novels.current_chapter AS 
-            chapter FROM full_novels JOIN novels ON full_novels.name = novels.name ORDER BY id`);
-            queriedData.data.fullNovels = rows;
-        }
-
-        if (updatedTime.leftOfShortNovel !== lastUpdatedTime.leftOfShortNovel) {
-            [rows, fields] = await promiseConn.query("SELECT image, title FROM short_novels WHERE short_novels.group = 'leftOfShortNovel'");
-            queriedData.data.leftOfShortNovel = rows[0];
-        }
-
-        if (updatedTime.rightOfShortNovels !== lastUpdatedTime.rightOfShortNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT image, title FROM short_novels WHERE short_novels.group = 'rightOfShortNovels' 
-            ORDER BY id`);
-            queriedData.data.rightOfShortNovels = rows;
-        }
-
-        if (updatedTime.reviewNovels !== lastUpdatedTime.reviewNovels) {
-            [rows, fields] = await promiseConn.query("SELECT image, title FROM review_novels ORDER BY id");
-            queriedData.data.reviewNovels = rows;
-        }
-
-        if (updatedTime.topGoodNovels !== lastUpdatedTime.topGoodNovels) {
-            [rows, fields] = await promiseConn.query("SELECT name AS title, view FROM novels ORDER BY view DESC LIMIT 10");
-            queriedData.data.topGoodNovels = rows;
-        }
-
-        if (updatedTime.newUpdateNovels !== lastUpdatedTime.newUpdateNovels) {
-            [rows, fields] = await promiseConn.query(`SELECT name AS title, view FROM novels WHERE first_time = last_time ORDER BY first_time 
-            DESC LIMIT 10`);
-            queriedData.data.newUpdateNovels = rows;
-        }
-
-        if (updatedTime.trendNovelsInMonth !== lastUpdatedTime.trendNovelsInMonth) {
-            [rows, fields] = await promiseConn.query("SELECT name FROM trend_novels_in_month ORDER BY id");
-            queriedData.data.trendNovelsInMonth = [];
-
-            for (let i = 0; i < rows.length; i++) {
-                queriedData.data.trendNovelsInMonth.push(rows[i].name);
-            }
-        }
-
-        pool.releaseConnection(conn);
-        const data = JSON.stringify(queriedData);
-        res.send(data);
-    });
+    conn.release();
+    const data = JSON.stringify(queriedData);
+    res.send(data);
 });
 
 app.listen(4000, () => {});
